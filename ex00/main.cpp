6f991052fc901eac64ada75d6e0fc6be99f87469
Arman Nikoyan <armannikoyan04@icloud.com>
2025-09-30 19:31:46 +0400

    added: subject tests

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: anikoyan <anikoyan@student.42yerevan.am>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 19:23:22 by anikoyan          #+#    #+#             */
-/*   Updated: 2025/09/30 19:28:29 by anikoyan         ###   ########.fr       */
+/*   Updated: 2025/09/30 19:31:01 by anikoyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -122,5 +122,22 @@ int main()
   test_edge_cases();
 
   std::cout << "\nAll tests passed successfully!\n";
+
+  {
+    std::cout << "\nSubject Tests\n";
+    int a = 2;
+    int b = 3;
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+  }
+
   return 0;
 }
